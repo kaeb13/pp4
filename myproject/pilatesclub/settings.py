9587c8981b10a613e0b6ba3ed9821f4032a754f9
kaeb13 <kaskebba@gmail.com>
2023-02-14 17:47:14 +0000

    Setting up django apps

--- myproject/pilatesclub/settings.py
+++ myproject/pilatesclub/settings.py
@@ -73,12 +73,36 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+#DATABASES = {
+#  'default': {
+     #   'ENGINE': 'django.db.backends.postgresql',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+   #     'USER': 'myusername',
+   #    'HOST': 'localhost',
+    #    'PORT': '3306',
+   # }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+
+     'default': {
+
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+         'NAME': 'hbjloiyd',
+
+         'USER': 'hbjloiyd',
+
+         'PASSWORD': 'Wsk3AvWS11vWjCDWwfz_paEVDwsfvjnN',
+
+         'HOST': 'mouse.db.elephantsql.com',
+
+         'PORT': '5432',
+
+     }
+ }
+
+
 
 
 # Password validation
